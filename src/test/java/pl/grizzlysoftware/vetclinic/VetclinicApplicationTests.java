6de83b15e1b115370dadefe274d2e8b34a119194
kuczynskil <l.kuczynski95@gmail.com>
2021-01-18 12:42:39 +0100

    Initial commit

---
+++ src/test/java/pl/grizzlysoftware/vetclinic/VetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.grizzlysoftware.vetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
