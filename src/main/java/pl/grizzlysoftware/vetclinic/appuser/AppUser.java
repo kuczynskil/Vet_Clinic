1171cdce02ea53eb150786a7b4f7222f153ecb9f
kuczynskil <l.kuczynski95@gmail.com>
2021-01-19 22:56:28 +0100

    Update AppUser class

--- src/main/java/pl/grizzlysoftware/vetclinic/appuser/AppUser.java
+++ src/main/java/pl/grizzlysoftware/vetclinic/appuser/AppUser.java
@@ -2,10 +2,7 @@ package pl.grizzlysoftware.vetclinic.appuser;
 
 import lombok.Getter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Pattern;
 
 @Entity
@@ -17,7 +14,12 @@ public class AppUser {
     private long id;
 
     @Pattern(regexp = "d{4}")
+    @Column(length = 4)
     private String loginID;
     @Pattern(regexp = "d{4}")
+    @Column(length = 4)
     private String loginPIN;
+
+    private String name;
+    private String surname;
 }
